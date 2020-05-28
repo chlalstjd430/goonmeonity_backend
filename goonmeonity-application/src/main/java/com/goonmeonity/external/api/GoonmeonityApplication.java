99aa8fd3e42136fec65b700238527afb7d8d2cb5
minseong choi <cms3136@gmail.com>
2020-05-29 00:52:38 +0900

    delete legacy import

--- goonmeonity-application/src/main/java/com/goonmeonity/external/api/GoonmeonityApplication.java
+++ goonmeonity-application/src/main/java/com/goonmeonity/external/api/GoonmeonityApplication.java
@@ -1,10 +1,8 @@
 package com.goonmeonity.external.api;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.goonmeonity")
