3a23b7c3292475b500d9ec331c88e887245d27d2
minseong choi <cms3136@gmail.com>
2020-05-29 00:46:26 +0900

    update scripts

--- goonmeonity-application/src/main/java/com/goonmeonity/external/api/GoonmeonityApplication.java
+++ goonmeonity-application/src/main/java/com/goonmeonity/external/api/GoonmeonityApplication.java
@@ -1,14 +1,17 @@
 package com.goonmeonity.external.api;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.goonmeonity")
 @EntityScan(basePackages = "com.goonmeonity")
 @EnableJpaRepositories(basePackages ="com.goonmeonity")
 public class GoonmeonityApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(GoonmeonityApplication.class, args);
     }
