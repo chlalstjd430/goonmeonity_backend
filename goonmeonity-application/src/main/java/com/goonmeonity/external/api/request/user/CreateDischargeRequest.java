cafc57654c87cdbf8c4e89fcf07b59c78f040b26
minseong choi <cms3136@gmail.com>
2020-06-05 17:49:45 +0900

    fix register discharge information api

--- goonmeonity-application/src/main/java/com/goonmeonity/external/api/request/user/CreateDischargeRequest.java
+++ goonmeonity-application/src/main/java/com/goonmeonity/external/api/request/user/CreateDischargeRequest.java
@@ -5,6 +5,7 @@ import com.goonmeonity.domain.entity.user.MilitaryStatus;
 import com.goonmeonity.external.api.error.ClientRequestIsInvalidError;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -12,29 +13,12 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class CreateDischargeRequest {
     private MilitaryStatus militaryStatus;
+
     private MilitaryAffiliate militaryAffiliate;
-    private int enlistmentYear;
-    private int enlistmentMonth;
-    private int enlistmentDay;
-    private int dischargeYear;
-    private int dischargeMonth;
-    private int dischargeDay;
 
-    public LocalDate getEnlistmentDate(){
-        try{
-            return LocalDate.of(enlistmentYear, enlistmentMonth, enlistmentDay);
-        }
-        catch (Exception e){
-            throw new ClientRequestIsInvalidError("enlistment date format is error");
-        }
-    }
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate enlistmentDate;
 
-    public LocalDate getDischargeDate(){
-        try{
-            return LocalDate.of(dischargeYear, dischargeMonth, dischargeDay);
-        }
-        catch (Exception e){
-            throw new ClientRequestIsInvalidError("discharge date format is error");
-        }
-    }
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dischargeDate;
 }
